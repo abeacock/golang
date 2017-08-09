0edb0d83ecfbd635c9d8b7ac162519ada37de50d
Andrew Beacock <github@andrewbeacock.com>
2017-08-09 08:16:26 +0100

    * Added about page handler(s) with an HTML and Markdown version (try /about or /aboutHtml)

--- serve/README.md
+++ serve/README.md
@@ -1,3 +1,3 @@
 # serve
 Starts a webserver on port 80 serving up the contents of the directory in which serve.exe was run.
-Copies the access URL to the clipboard as well as printing it out to the command line. 
+Copies the access URL to the clipboard as well as printing it out to the command line.
--- serve/main.go
+++ serve/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 
 	"github.com/atotto/clipboard"
+	"github.com/russross/blackfriday"
 )
+
+var data = map[string]string{
+	"author": "Andrew Beacock",
+}
+
+func aboutHtmlFunc(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.New("about").Parse(aboutHtml)
+	t.Execute(w, data)
+}
+
+func aboutMarkdownFunc(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.New("about").Parse(aboutMarkdown)
+	var buffer bytes.Buffer
+	t.Execute(&buffer, data)
+	w.Write(blackfriday.MarkdownBasic(buffer.Bytes()))
+}
 
 func main() {
 	name, _ := os.Hostname()
@@ -16,5 +35,19 @@ func main() {
 	fmt.Println(url)
 	clipboard.WriteAll(string(url))
 
-	http.ListenAndServe(":"+port, http.FileServer(http.Dir(".")))
+	http.HandleFunc("/about", aboutMarkdownFunc)
+	http.HandleFunc("/aboutHtml", aboutHtmlFunc)
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
+	http.ListenAndServe(":"+port, nil)
 }
+
+const aboutHtml string = `
+<h1>About</h1>
+<p>The author of serve.go is {{.author}}</p>
+`
+
+const aboutMarkdown string = `
+# About
+The author of serve.go is {{.author}}
+`
