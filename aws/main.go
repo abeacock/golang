ac72d1ee1b9cd46b5557c8ce60583ec6f5157e63
Andrew Beacock <github@andrewbeacock.com>
2019-12-03 12:14:15 +0000

    Added cloudwatch output of the entire event

--- aws/main.go
+++ aws/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+  "encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,7 +12,7 @@ import (
 	"os"
 )
 
-func HandleRequest(ctx context.Context, event events.IoTButtonEvent) (string, error) {
+func HandleRequest(ctx context.Context, event events.IoTButtonEvent) (error) {
 	fmt.Println("creating session")
 	sess := session.Must(session.NewSession())
 	fmt.Println("session created")
@@ -34,7 +35,14 @@ func HandleRequest(ctx context.Context, event events.IoTButtonEvent) (string, er
 	// Pretty-print the response data.
 	fmt.Println(resp)
 
-	return fmt.Sprintf("Hello %s!", event.SerialNumber), nil
+  eventAsJson, err := json.Marshal(event)
+    if err != nil {
+        panic (err)
+    }
+
+	fmt.Printf("Here is the full event: %s", string(eventAsJson))
+
+  return nil
 }
 
 func main() {
