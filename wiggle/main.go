7a235afe723f37d84e79c0bda4d4294206bd3f89
Andrew Beacock <github@andrewbeacock.com>
2019-12-12 12:07:11 +0000

    Wiggle moves the mouse around randomly every 5 seconds

---
+++ wiggle/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/go-vgo/robotgo"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	x, y := robotgo.GetScreenSize()
+
+	for true {
+		robotgo.MoveMouseSmooth(rand.Intn(x), rand.Intn(y))
+		time.Sleep(time.Second * 5)
+	}
+}
